ca880bcc6577e75d0dc2a6097a922476fa24d4f8
Willian Barbosa <williampgm@gmail.com>
2022-03-24 15:30:23 -0300

    Import CSV File

--- src/modules/cars/services/SpecificationService.ts
+++ src/modules/cars/services/SpecificationService.ts
@@ -52,6 +52,7 @@ class SpecificationService {
           specifications.push({ name, description });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(specifications);
         })
         .on("error", (err) => {
